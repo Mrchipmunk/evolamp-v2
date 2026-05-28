eafc0be495e11e2847691b70ec4d5799d1892585
mrchipmunk <66779513+Mrchipmunk@users.noreply.github.com>
2026-05-28 19:04:54 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,20 @@ radio.onReceivedString(function (receivedString) {
         CONNECTED = true
     }
     if (CONNECTED == true) {
-    	
+        if (receivedString == "LAMPOFF") {
+            LIGHTSTATE = false
+            pins.digitalWritePin(DigitalPin.P0, 0)
+        } else if (receivedString == "LAMPON") {
+            LIGHTSTATE = true
+            pins.digitalWritePin(DigitalPin.P0, 1)
+        } else if (false) {
+        	
+        } else {
+        	
+        }
     }
 })
+let LIGHTSTATE = false
 let CONNECTED = false
 basic.showString("EVOTEST")
 let RADIOGROUP = 1
