d8974f3e1de72495dc12887bf531b5cb4d70f128
mrchipmunk <66779513+Mrchipmunk@users.noreply.github.com>
2026-05-28 21:06:16 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    if (CRAMODE == false) {
+        if (LIGHTSTATE == 1) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            LIGHTSTATE = 0
+        } else if (LIGHTSTATE == 0) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            LIGHTSTATE = 1
+        }
+    }
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "CONNECT") {
         radio.sendString("CONNECTED")
@@ -5,24 +16,46 @@ radio.onReceivedString(function (receivedString) {
     }
     if (CONNECTED == true) {
         if (receivedString == "LAMPOFF") {
-            LIGHTSTATE = false
+            LIGHTSTATE = 0
             pins.digitalWritePin(DigitalPin.P0, 0)
         } else if (receivedString == "LAMPON") {
-            LIGHTSTATE = true
+            LIGHTSTATE = 1
             pins.digitalWritePin(DigitalPin.P0, 1)
-        } else if (false) {
-        	
+        } else if (receivedString == "CRAON") {
+            LIGHTSTATE = 2
+            CRAMODE = true
         } else {
         	
         }
     }
 })
-let LIGHTSTATE = false
+function cramode () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wawawawaa), music.PlaybackMode.LoopingInBackground)
+    while (CRAMODE == true) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Tortoise)
+        basic.showIcon(IconNames.StickFigure)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.showIcon(IconNames.Skull)
+        basic.showIcon(IconNames.Umbrella)
+        basic.showIcon(IconNames.Silly)
+    }
+    music.stopAllSounds()
+}
+let CRAMODE = false
+let LIGHTSTATE = 0
 let CONNECTED = false
 basic.showString("EVOTEST")
+music.setBuiltInSpeakerEnabled(true)
+pins.setAudioPinEnabled(false)
 let RADIOGROUP = 1
 CONNECTED = false
+LIGHTSTATE = 0
+CRAMODE = true
 radio.setGroup(RADIOGROUP)
+cramode()
 basic.forever(function () {
 	
 })
